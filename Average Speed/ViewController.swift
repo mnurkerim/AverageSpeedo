403fb0c444aa6624b57f066bb5d5a0e9641df6cb
Mo Nurkerim <muhammednurkerim@gmail.com>
2017-05-30 12:35:42 +0100

    Initial Commit

---
+++ Average Speed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Average Speed
+//
+//  Created by Muhammed Nurkerim on 30/05/2017.
+//  Copyright © 2017 Muhammed Nurkerim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
