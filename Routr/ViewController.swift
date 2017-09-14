7359a38cc2fce4379fbcdd729ee88522bb2210e5
Mo Nurkerim <muhammednurkerim@gmail.com>
2017-09-14 21:34:42 +0100

    Refactored code and made some minor modifications to the location manager distance filter

--- Routr/ViewController.swift
+++ Routr/ViewController.swift
@@ -25,13 +25,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var miniStartButton: UIButton!
     @IBOutlet weak var resetButton: UIButton!
 
+    var savedDrive: Drive?
+    
     var seconds = 0.0
     var distance = Measurement(value: 0, unit: UnitLength.meters)
     
     var miniSeconds = 0.0
     var miniDistance = Measurement(value: 0, unit: UnitLength.meters)
-
-    var savedDrive: Drive?
 
     private let locationManager = LocationManager.shared
 
@@ -48,52 +48,6 @@ class ViewController: UIViewController {
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-    }
-
-    func eachSecond() {
-        seconds += 1
-        updateDisplay()
-    }
-    
-    func miniEachSecond() {
-        miniSeconds += 1
-        updateMiniDisplay()
-    }
-    
-    private func updateMiniDisplay() {
-        let formattedDistance = FormatDisplay.distance(miniDistance)
-        let formattedTime = FormatDisplay.time(Int(miniSeconds))
-        
-        miniDistanceLabel.text = "\(formattedDistance)"
-        miniTimeLabel.text = "\(formattedTime)"
-        miniAvgSpeedLabel.text = String(format: "%.2f mph", ((miniDistance.value / miniSeconds) * 2.23693629))
-    }
-    
-    private func updateDisplay() {
-        let formattedDistance = FormatDisplay.distance(distance)
-        let formattedTime = FormatDisplay.time(Int(seconds))
-        let formattedPace = FormatDisplay.pace(distance: distance,
-                                               seconds: Int(seconds),
-                                               outputUnit: UnitSpeed.minutesPerMile)
-        
-        distanceLabel.text = "\(formattedDistance)"
-        timeLabel.text = "\(formattedTime)"
-        paceLabel.text = "\(formattedPace)"
-        averageSpeedLabel.text = String(format: "%.2f mph", ((distance.value / seconds) * 2.23693629))
-    }
-    
-    private func resetUiValues() {
-        averageSpeedLabel.text = "0.00"
-        distanceLabel.text = "0.00"
-        timeLabel.text = "0:00:00"
-        paceLabel.text = "0.00"
-        speedLabel.text = "0.00"
-    }
-    
-    private func resetMiniUiValues() {
-        miniTimeLabel.text = "0:00:00"
-        miniAvgSpeedLabel.text = "0.00 mph"
-        miniDistanceLabel.text = "0.00"
     }
 
     override func viewDidLoad() {
@@ -113,68 +67,19 @@ class ViewController: UIViewController {
         super.viewDidAppear(animated)
     }
 
-    private func startLocationUpdates() {
-        locationManager.delegate = self
-        locationManager.activityType = .automotiveNavigation
-        locationManager.distanceFilter = 5
-        locationManager.startUpdatingLocation()
-    }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func miniStartPressed() {
-        miniStartButton.isHidden = true
-        resetButton.isHidden = false
-        
-        miniTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.miniEachSecond), userInfo: nil, repeats: true)
-        
-        miniSeconds = 0.0
-        miniDistance = Measurement(value: 0, unit: UnitLength.meters)
+    func eachSecond() {
+        seconds += 1
+        updateDisplay()
     }
     
-    @IBAction func miniResetPressed() {
-        miniStartButton.isHidden = false
-        resetButton.isHidden = true
-        
-        miniTimer.invalidate()
-        
-        miniSeconds = 0.0
-        miniDistance = Measurement(value: 0, unit: UnitLength.meters)
-    }
-
-    @IBAction func startPressed() {
-        startButton.isHidden = true
-        stopButton.isHidden = false
-
-        locations.removeAll(keepingCapacity: false)
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.eachSecond), userInfo: nil, repeats: true)
-        startLocationUpdates()
-
-        seconds = 0.0
-        distance = Measurement(value: 0, unit: UnitLength.meters)
-    }
-
-    @IBAction func stopPressed() {
-        stopButton.isHidden = true
-        startButton.isHidden = false
-
-        let alertController = UIAlertController(title: "Tracking Stopped", message: "Save or discard this session?", preferredStyle: .actionSheet)
-        let saveAction = UIAlertAction(title: "Save", style: .default, handler: saveActionHandler)
-        let discardAction = UIAlertAction(title: "Discard", style: .destructive, handler: discardActionHandler)
-        let resumeAction = UIAlertAction(title: "Resume", style: .default, handler: { _ in
-            self.stopButton.isHidden = false
-            self.startButton.isHidden = true
-            
-            self.startLocationUpdates()
-        })
-        alertController.addAction(saveAction)
-        alertController.addAction(resumeAction)
-        alertController.addAction(discardAction)
-
-        self.present(alertController, animated: true, completion: nil)
+    func miniEachSecond() {
+        miniSeconds += 1
+        updateMiniDisplay()
     }
 
     func saveDrive() {
@@ -219,6 +124,101 @@ class ViewController: UIViewController {
         
         self.navigationController?.popToRootViewController(animated: true)
     }
+    
+    private func startLocationUpdates() {
+        locationManager.delegate = self
+        locationManager.activityType = .automotiveNavigation
+        locationManager.distanceFilter = 10
+        locationManager.startUpdatingLocation()
+    }
+    
+    private func updateMiniDisplay() {
+        let formattedDistance = FormatDisplay.distance(miniDistance)
+        let formattedTime = FormatDisplay.time(Int(miniSeconds))
+        
+        miniDistanceLabel.text = "\(formattedDistance)"
+        miniTimeLabel.text = "\(formattedTime)"
+        miniAvgSpeedLabel.text = String(format: "%.2f mph", ((miniDistance.value / miniSeconds) * 2.23693629))
+    }
+    
+    private func updateDisplay() {
+        let formattedDistance = FormatDisplay.distance(distance)
+        let formattedTime = FormatDisplay.time(Int(seconds))
+        let formattedPace = FormatDisplay.pace(distance: distance,
+                                               seconds: Int(seconds),
+                                               outputUnit: UnitSpeed.minutesPerMile)
+        
+        distanceLabel.text = "\(formattedDistance)"
+        timeLabel.text = "\(formattedTime)"
+        paceLabel.text = "\(formattedPace)"
+        averageSpeedLabel.text = String(format: "%.2f mph", ((distance.value / seconds) * 2.23693629))
+    }
+    
+    private func resetUiValues() {
+        averageSpeedLabel.text = "0.00"
+        distanceLabel.text = "0.00"
+        timeLabel.text = "0:00:00"
+        paceLabel.text = "0.00"
+        speedLabel.text = "0.00"
+    }
+    
+    private func resetMiniUiValues() {
+        miniTimeLabel.text = "0:00:00"
+        miniAvgSpeedLabel.text = "0.00 mph"
+        miniDistanceLabel.text = "0.00"
+    }
+    
+    @IBAction func miniStartPressed() {
+        miniStartButton.isHidden = true
+        resetButton.isHidden = false
+        
+        miniTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.miniEachSecond), userInfo: nil, repeats: true)
+        
+        miniSeconds = 0.0
+        miniDistance = Measurement(value: 0, unit: UnitLength.meters)
+    }
+    
+    @IBAction func miniResetPressed() {
+        miniStartButton.isHidden = false
+        resetButton.isHidden = true
+        
+        miniTimer.invalidate()
+        
+        miniSeconds = 0.0
+        miniDistance = Measurement(value: 0, unit: UnitLength.meters)
+    }
+    
+    @IBAction func startPressed() {
+        startButton.isHidden = true
+        stopButton.isHidden = false
+        
+        locations.removeAll(keepingCapacity: false)
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.eachSecond), userInfo: nil, repeats: true)
+        startLocationUpdates()
+        
+        seconds = 0.0
+        distance = Measurement(value: 0, unit: UnitLength.meters)
+    }
+    
+    @IBAction func stopPressed() {
+        stopButton.isHidden = true
+        startButton.isHidden = false
+        
+        let alertController = UIAlertController(title: "Tracking Stopped", message: "Save or discard this session?", preferredStyle: .actionSheet)
+        let saveAction = UIAlertAction(title: "Save", style: .default, handler: saveActionHandler)
+        let discardAction = UIAlertAction(title: "Discard", style: .destructive, handler: discardActionHandler)
+        let resumeAction = UIAlertAction(title: "Resume", style: .default, handler: { _ in
+            self.stopButton.isHidden = false
+            self.startButton.isHidden = true
+            
+            self.startLocationUpdates()
+        })
+        alertController.addAction(saveAction)
+        alertController.addAction(resumeAction)
+        alertController.addAction(discardAction)
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
 }
 
 // MARK: - CLLocationManagerDelegate
@@ -226,15 +226,13 @@ extension ViewController: CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         for newLocation in locations {
-            guard newLocation.horizontalAccuracy < 40 else { continue }
+//            guard newLocation.horizontalAccuracy < 40 else { continue }
             
             if let lastLocation = self.locations.last {
                 let delta = newLocation.distance(from: lastLocation)
                 distance = distance + Measurement(value: delta, unit: UnitLength.meters)
                 miniDistance = miniDistance + Measurement(value: delta, unit: UnitLength.meters)
                 speedLabel.text = String(format:"%.1f mph", (newLocation.speed * 2.23693629))
-            } else {
-                speedLabel.text = "0.00 mph"
             }
             
             self.locations.append(newLocation)
